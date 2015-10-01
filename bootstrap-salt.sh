b64bd0662c23b6459783faf14f4ef0ef9d8a251e
Robert James Hernandez <l2ol33rt@gmail.com>
2015-10-01 11:35:06 -0700

    Pip -a fixes when virtualenv not defined

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -353,11 +353,7 @@ do
          ;;
     s )  __DEFAULT_SLEEP="$OPTARG"                      ;;
     V )  _INSTALL_2_VIRTUALENV="$OPTARG"
-         if [ -d "$_INSTALL_2_VIRTUALENV" ]; then
-             echoerror "The directory ${_INSTALL_2_VIRTUALENV} for virtualenv already exists"
-             exit 1
-         fi
-         _PIP_ALLOWED=$BS_TRUE
+
          ;;
     a )  _PIP_ALL=$BS_TRUE                              ;;
     M )  _INSTALL_MASTER=$BS_TRUE                       ;;
@@ -519,6 +515,19 @@ fi
 if [ ${_DISABLE_SALT_CHECKS} -eq 0 ]; then
     [ -f /tmp/disable_salt_checks ] && _DISABLE_SALT_CHECKS=$BS_TRUE && \
         echowarn "Found file: /tmp/disable_salt_checks, setting \$_DISABLE_SALT_CHECKS=true"
+fi
+
+# Because -a can only be installed into virtualenv
+if ([ $_PIP_ALL -eq $BS_TRUE ] && [ $_INSTALL_2_VIRTUALENV = "null" ]); then
+    # Could possibly set up a default virtualenv location when -a flag is passed
+    echoerror "-V(virtualenv) dir required for -a (pip pkg) installs"
+    exit 1
+fi
+
+# Make sure virtualenv directory does not already exist
+if [ -d "$_INSTALL_2_VIRTUALENV" ]; then
+    echoerror "The directory ${_INSTALL_2_VIRTUALENV} for virtualenv already exists"
+    exit 1
 fi
 
 echoinfo "${CALLER} ${0} -- Version ${__ScriptVersion}"
@@ -1836,7 +1845,7 @@ __install_pip_deps() {
 
     requirements_file=$1
     if [ ! -f "${requirements_file}" ]; then
-        echoerror "Requirements file: ${requirements_file} does not exist, needed for -a (pip pkg) installs"
+        echoerror "Requirements file: ${requirements_file} cannot be found, needed for -a (pip pkg) installs"
         exit 1
     fi
 
@@ -2159,7 +2168,7 @@ install_ubuntu_git_deps() {
         # Get just the apt packages that are required to build all the pythons
         __apt_get_install_noinput $__PACKAGES || return 1
         # Install the pythons from requirements (only zmq for now)
-        install_pip_deps "${__SALT_GIT_CHECKOUT_DIR}/requirements/zeromq.txt" || return 1
+        __install_pip_deps "${__SALT_GIT_CHECKOUT_DIR}/requirements/zeromq.txt" || return 1
     else
         install_ubuntu_deps || return 1
         __apt_get_install_noinput python-yaml python-m2crypto python-crypto \
